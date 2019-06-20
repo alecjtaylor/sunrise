7e700643749b382550d9d3e84b53551d9edbd9df
Alec Clancy-Taylor <alecjtaylor@gmail.com>
2019-06-20 22:05:16 +0100

    updated datetime object

--- timelapse.py
+++ timelapse.py
@@ -6,23 +6,25 @@ f = open("sunrise.json", 'r')
 sunriseDict = json.load(f)
 f.close
 
-
-# Set time and date to now so can be used later!
-timeNow = datetime.utcnow().strftime("%H:%M:%S")
-dateNow = datetime.utcnow().strftime("%Y-%m-%d")
+# Set time and date to now so can be used later (everything in GMT)!
+timeNow = datetime.utcnow()#.strftime("%H:%M:%S")
+dateNow = datetime.utcnow()#.strftime("%Y-%m-%d")
 
 # grab times as strings from dict using todays date as a key - the value is a list containing sunrise and sunset
-sunrise = sunriseDict[dateNow][0]
-sunset = sunriseDict[dateNow][1]
+sunrise = str(sunriseDict[dateNow][0])
+sunriseRaw = datetime.strptime(sunrise, "%H:%M:%S")
+
+sunset = str(sunriseDict[dateNow][1])
+sunsetRaw = datetime.strptime(sunset, "%H:%M:%S")
 
 #Check to see if the time objects can be used in calcs (comparisons didn't work as there was no date element to the sunrise and sunset times)
-if timeNow >= sunrise:
+if timeNow >= sunriseRaw and timeNow < sunsetRaw:
     print 
-    print "yes, take a photo,  the time is now " + timeNow + " and sunrise was " + sunrise + " and the sunset is at " + sunset
+    print "yes, take a photo,  the time is now " + timeNow.strftime("%H:%M:%S") + " and sunrise was " + sunrise + " and the sunset is at " + sunset
     print
-elif timenow >= sunset:
+elif timeNow <= sunriseRaw or timeNow >= sunsetRaw:
     print
-    print "no, take a long exposure, the time is now " + timeNow + "and the sunset was at " + sunset
+    print "no, take a long exposure, the time is now " + timeNow.strftime("%H:%M:%S") + " and the sunset was at " + sunset
     print
 else:
     print "Something has gone wrong!"
