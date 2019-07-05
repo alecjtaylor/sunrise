bfc547cccfafdc22cd6da028f8d1a6c21290ac45
Alec Clancy-Taylor <alecjtaylor@gmail.com>
2019-07-05 13:10:03 +0100

    update

--- timelapse.py
+++ timelapse.py
@@ -1,4 +1,4 @@
-import os, json, ast
+import os, json, ast, time
 from datetime import datetime, timedelta
 
 # encoded as unicode for some reason - not sure if this will be an issue later on
@@ -6,47 +6,34 @@ f = open("sunrise.json", 'r')
 sunriseDict = json.load(f)
 f.close
 
-#### Start Loop Here ####
+    #### Start Loop Here ####
+try:   
+    while True:
+            
+        # Set time and date to now so can be used later (everything in GMT)!
+        timeNow = datetime.utcnow()
+        dateNow = datetime.utcnow().strftime("%Y-%m-%d")
 
-# Set time and date to now so can be used later (everything in GMT)!
-timeNow = datetime.utcnow()
-dateNow = datetime.utcnow().strftime("%Y-%m-%d")
+        # grab times as strings from dict using todays date as a key - the value is a list containing sunrise and sunset strings which need to be converted to datetime objects
+        sunrise = str(sunriseDict[dateNow][0])
+        sunset = str(sunriseDict[dateNow][1])
 
-# grab times as strings from dict using todays date as a key - the value is a list containing sunrise and sunset strings which need to be converted to datetime objects
-sunrise = str(sunriseDict[dateNow][0])
-# Convert to datetime object
-sunriseRaw = datetime.strptime(sunrise, "%Y-%m-%d %H:%M:%S")
+        # Convert to datetime object
+        sunriseRaw = datetime.strptime(sunrise, "%Y-%m-%d %H:%M:%S")
+        sunsetRaw = datetime.strptime(sunset, "%Y-%m-%d %H:%M:%S")
 
-sunset = str(sunriseDict[dateNow][1])
-# Convert to datetime object
-sunsetRaw = datetime.strptime(sunset, "%Y-%m-%d %H:%M:%S")
+        if timeNow >= sunriseRaw and timeNow < sunsetRaw:
 
-#### Debugging code here ####
+            print "Greater than sunrise time and less than sunset time"
+            print "Take normal timelapse and wait 10 seconds until next image"
 
-timeNow = timeNow - timedelta(hours = 0)
+            time.sleep(10)
+        
+        elif timeNow < sunriseRaw or timeNow >= sunsetRaw:
 
-print timeNow
-print sunsetRaw
+            print "Less than sunrise or greater than sunset"
+            print "Take long exposure images"
 
-
-# # This loop is shit - it needs to be simple and only have two options
-
-
-# if timeNow >= sunsetRaw:# and timeNow < sunsetRaw:
-#     print "Greater than sunset time"
-# elif timeNow <= sunriseRaw: #or timeNow >= sunsetRaw:
-#    print "Before sunrise time"
-# elif timeNow >= sunriseRaw:# and timeNow < sunsetRaw:
-#     print "After sunrise time"
-# else:
-#     print "Something has gone wrong!"
-
-
-# print " testing of fancy new loop"
-
-if timeNow >= sunriseRaw and timeNow < sunsetRaw:
-    print "Greater than sunrise time and less than sunset time"
-    print "Take normal timelapse and wait 10 seconds until next image"
-elif timeNow < sunriseRaw or timeNow >= sunsetRaw:
-    print "Less than sunrise or greater than sunset"
-    print "Take long exposure images"
+            time.sleep(60)
+except KeyboardInterrupt:
+    print('interrupted!')
